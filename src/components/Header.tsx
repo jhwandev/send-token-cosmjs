7d39a94839933b0da1011af560c07b90acaeddec
jhwandev <jhwan.dev@gmail.com>
2023-02-17 20:43:06 +0900

    디자인 버그 fix - 정렬오류

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,24 +24,25 @@ function Header() {
 
   return (
     <div className="h-[25vh] text-white p-10">
-      <div className="mb-10 flex justify-center font-bold text-2xl">
+      <div className="mb-7 flex justify-center items-center font-bold text-2xl">
         <Airplane className="w-10" />
-        &nbsp; Token Sender
+        &nbsp;Token Sender
       </div>
       <div className="flex justify-center">
         <Link
           to="/sendNova"
           className={
-            "min-w-[100px] text-center hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300 mr-3 py-4 px-3 text-white rounded font-bold " +
+            "min-w-[100px] text-center hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300 py-4 text-white rounded font-bold " +
             ("nova" === btnActive ? "bg-blue-500 rounded" : "")
           }
         >
           NOVA
         </Link>
+        <div className="w-3"></div>
         <Link
           to="/sendAtom"
           className={
-            "min-w-[100px] text-center hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 mr-3 py-4 px-3 text-white rounded font-bold " +
+            "min-w-[100px] text-center hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 py-4 text-white rounded font-bold " +
             ("atom" === btnActive ? "bg-violet-500 rounded" : "")
           }
         >
