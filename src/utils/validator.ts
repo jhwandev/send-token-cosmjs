9c2e2f0f372528c178042c02c2b029b3ee9b2420
jhwandev <jhwan.dev@gmail.com>
2023-02-18 15:40:05 +0900

    유효성검사 다국어기능 반영

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -3,22 +3,21 @@
  * @param mnemonic
  * @returns
  */
-export const isValidMnemonic = (mnemonic: string): boolean => {
+export const isValidMnemonic = (mnemonic: string): string => {
   // 알파벳과 공백 이외에 다른 문자 있는지 확인
   const alphabetSpace = RegExp(/[^a-z\s]/);
-  const isNotValid = alphabetSpace.test(mnemonic);
+  const isNotValidType = alphabetSpace.test(mnemonic);
 
-  if (isNotValid) return false;
+  if (isNotValidType) return "valid.mnemonic.01";
 
-  //갯수확인
   const nemonicSplit = mnemonic.split(" ");
   const nemonicLength = nemonicSplit.length;
 
   // 12개 또는 24개 니모닉인지 확인
   if (nemonicLength === 24 || nemonicLength === 12) {
-    return true;
+    return "";
   } else {
-    return false;
+    return "valid.mnemonic.02";
   }
 };
 
@@ -27,18 +26,20 @@ export const isValidMnemonic = (mnemonic: string): boolean => {
  * @param address
  * @returns
  */
-export const isValidAddress = (address: string, prefix: string): boolean => {
+export const isValidAddress = (address: string, prefix: string): string => {
   // prefix 일치여부 확인
-  var splittedAddress = address.split(prefix);
-  if (splittedAddress.length !== 2) return false;
-  // 길이확인 - prefix제외 39자리
-  if (splittedAddress[1].length !== 39) return false;
-  // 소문자 알파벳과 숫자 이외에 다른 문자 있는지 확인
-  var eee = RegExp(/[^a-z0-9]/);
-  const isValid = eee.test(address);
-  if (isValid) return false;
+  const splittedAddress = address.split(prefix);
+  if (splittedAddress.length !== 2) return "valid.receiverAddress.01";
 
-  return true;
+  // 소문자 알파벳과 숫자 이외에 다른 문자 있는지 확인
+  const alphabetNumber = RegExp(/[^a-z0-9]/);
+  const isNotValidType = alphabetNumber.test(address);
+  if (isNotValidType) return "valid.receiverAddress.02";
+
+  // 길이확인 - prefix제외 39자리
+  if (splittedAddress[1].length !== 39) return "valid.receiverAddress.03";
+
+  return "";
 };
 
 /**
@@ -46,18 +47,21 @@ export const isValidAddress = (address: string, prefix: string): boolean => {
  * @param amount
  * @returns
  */
-export const isValidAmount = (value: string): boolean => {
+export const isValidAmount = (value: string): string => {
   const amount = Number(value);
   // 0과 같거나 보다 작은 수 입력 불가
-  if (amount <= 0) return false;
-  // 14자리 이상 입력 불가
-  if (value.length > 14) return false;
-  // 숫자만 입력 가능
-  if (Number.isNaN(amount)) return false;
-  // 소수점 6자리 이상 입력 불가
-  if (Number.isInteger(amount * 10 ** 6) === false) return false;
+  if (amount <= 0) return "valid.amount.01";
 
-  return true;
+  // 14자리 이상 입력 불가
+  if (value.length > 14) return "valid.amount.02";
+
+  // 숫자만 입력 가능
+  if (Number.isNaN(amount)) return "valid.amount.03";
+
+  // 소수점 6자리 이상 입력 불가
+  if (Number.isInteger(amount * 10 ** 6) === false) return "valid.amount.04";
+
+  return "";
 };
 
 /**
@@ -65,15 +69,16 @@ export const isValidAmount = (value: string): boolean => {
  * @param value
  * @returns
  */
-export const isValidAmountForChangeEvent = (value: string): boolean => {
+export const isValidAmountForChangeEvent = (value: string): string => {
   const amount = Number(value);
+
   // 14자리 이상 입력 불가
-  if (value.length > 14) return false;
+  if (value.length > 14) return "valid.amount.02";
   // 숫자만 입력 가능
-  if (Number.isNaN(amount)) return false;
+  if (Number.isNaN(amount)) return "valid.amount.03";
   // 소수점 6자리 이상 입력 불가
-  if (Number.isInteger(amount * 10 ** 6) === false) return false;
-  // "0"다음에는 "."만 입력 가능
+  if (Number.isInteger(amount * 10 ** 6) === false) return "valid.amount.04";
+  // "0"다음에는 "."만 입력 가능 - 편의상 제거
   // if (
   //   value.length > 1 &&
   //   value.split("")[0] === "0" &&
@@ -81,5 +86,5 @@ export const isValidAmountForChangeEvent = (value: string): boolean => {
   // ) {
   //   return false;
   // }
-  return true;
+  return "";
 };
